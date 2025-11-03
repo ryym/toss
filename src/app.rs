d0fac757ce32ec101ffbc940f0cbb0ead8cb6557
ryym <ryym.64@gmail.com>
2025-11-03 14:00:03 +0900

    feat(app): Replace Window by Pager
    to be able to handle large source files.

--- src/app.rs
+++ src/app.rs
@@ -2,17 +2,17 @@
 mod tests;
 
 use std::fs::File;
-use std::io::IsTerminal;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, IsTerminal};
 use std::time::Duration;
 use std::{env, panic, thread};
 
 use crate::error::AnyError;
-use crate::lines::Line;
 use crate::logger;
+use crate::pager::{PageSize, Pager};
+use crate::reader::Reader;
 use crate::screen::Screen;
 use crate::screen::{Event, Key};
-use crate::window::Window;
+use crate::source::{self, Source};
 
 pub fn run() -> Result<(), AnyError> {
     let _guard = logger::setup_file_logger()?;
@@ -22,31 +22,36 @@ pub fn run() -> Result<(), AnyError> {
 }
 
 fn run_with<S: Screen>(screen: &mut S, args: Vec<String>) -> Result<(), AnyError> {
+    let size = screen.size()?;
     let stdin = io::stdin().lock();
-    let lines: Vec<String> = if stdin.is_terminal() {
+    if stdin.is_terminal() {
         let file_path = args.first().unwrap();
         let file = File::open(file_path)?;
-        let reader = BufReader::new(file);
-        reader.lines().map(|l| l.unwrap()).collect()
+        let source = source::as_seekable(file);
+        let reader = Reader::new(source);
+        let pager = Pager::new(reader, PageSize::new(size.n_rows(), size.n_cols()))?;
+        if let Some(pager) = pager {
+            return App::new(screen, pager).run();
+        }
     } else {
-        let reader = BufReader::new(stdin);
-        reader.lines().map(|l| l.unwrap()).collect()
-    };
-
-    let size = screen.size()?;
-    let lines = lines.into_iter().map(Line::new).collect();
-    let window = Window::new(size.n_cols(), size.n_rows(), lines);
-    App::new(screen, window).run()
+        let source = source::as_readable(stdin);
+        let reader = Reader::new(source);
+        let pager = Pager::new(reader, PageSize::new(size.n_rows(), size.n_cols()))?;
+        if let Some(pager) = pager {
+            return App::new(screen, pager).run();
+        }
+    }
+    Ok(())
 }
 
-struct App<'s, S: Screen> {
+struct App<'s, S, R, Src> {
     screen: &'s mut S,
-    window: Window,
+    pager: Pager<R, Src>,
 }
 
-impl<'s, S: Screen> App<'s, S> {
-    fn new(screen: &'s mut S, window: Window) -> Self {
-        Self { screen, window }
+impl<'s, S: Screen, R, Src: Source<R>> App<'s, S, R, Src> {
+    fn new(screen: &'s mut S, pager: Pager<R, Src>) -> Self {
+        Self { screen, pager }
     }
 
     fn run(&mut self) -> Result<(), AnyError> {
@@ -69,24 +74,24 @@ impl<'s, S: Screen> App<'s, S> {
                             }
                         }
                         'g' => {
-                            self.window.seek_to_start();
+                            self.pager.scroll_to_start()?;
                             self.draw_rows()?;
                         }
                         'G' => {
-                            self.window.seek_to_end();
+                            self.pager.scroll_to_end()?;
                             self.draw_rows()?;
                         }
                         'd' => {
-                            self.smooth_scroll(self.window.n_rows() / 2, true)?;
+                            self.smooth_scroll(self.pager.size().rows() / 2, true)?;
                         }
                         'u' => {
-                            self.smooth_scroll(self.window.n_rows() / 2, false)?;
+                            self.smooth_scroll(self.pager.size().rows() / 2, false)?;
                         }
                         'f' | ' ' => {
-                            self.smooth_scroll(self.window.n_rows(), true)?;
+                            self.smooth_scroll(self.pager.size().rows(), true)?;
                         }
                         'b' => {
-                            self.smooth_scroll(self.window.n_rows(), false)?;
+                            self.smooth_scroll(self.pager.size().rows(), false)?;
                         }
                         _ => continue,
                     },
@@ -105,7 +110,7 @@ impl<'s, S: Screen> App<'s, S> {
         self.screen.clear()?;
 
         let mut i_row = 0;
-        for row_span in self.window.row_spans() {
+        for row_span in self.pager.row_spans() {
             self.screen.draw_at(i_row, row_span.line())?;
             i_row += row_span.size();
         }
@@ -115,24 +120,27 @@ impl<'s, S: Screen> App<'s, S> {
     }
 
     fn scroll_forward_oneline(&mut self) -> Result<bool, AnyError> {
-        if !self.window.scroll_down_one_row() {
-            return Ok(false);
+        let row_size = self.pager.size().rows();
+        match self.pager.scroll_down_one_row()? {
+            None => Ok(false),
+            Some(new_row_span) => {
+                self.screen.scroll_forward(1)?;
+                let row_start = row_size - new_row_span.size();
+                self.screen.draw_at(row_start, new_row_span.line())?;
+                Ok(true)
+            }
         }
-        self.screen.scroll_forward(1)?;
-        let new_row_span = self.window.end_row_span();
-        let row_span_start = self.window.n_rows() - new_row_span.size();
-        self.screen.draw_at(row_span_start, new_row_span.line())?;
-        Ok(true)
     }
 
     fn scroll_backword_oneline(&mut self) -> Result<bool, AnyError> {
-        if !self.window.scroll_up_one_row() {
-            return Ok(false);
+        match self.pager.scroll_up_one_row()? {
+            None => Ok(false),
+            Some(new_row_span) => {
+                self.screen.scroll_backward(1)?;
+                self.screen.draw_at(0, new_row_span.line())?;
+                Ok(true)
+            }
         }
-        self.screen.scroll_backward(1)?;
-        let new_row_span = self.window.start_row_span();
-        self.screen.draw_at(0, new_row_span.line())?;
-        Ok(true)
     }
 
     fn smooth_scroll(&mut self, n_rows: usize, go_down: bool) -> Result<(), AnyError> {
