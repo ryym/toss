321781bb1c5ea4ec1f936b759b65b6d8e74af075
ryym <ryym.64@gmail.com>
2025-11-03 13:57:05 +0900

    feat: Add Pager which loads lines to page

---
+++ src/pager.rs
@@ -0,0 +1,210 @@
+use crate::{
+    error::AnyError,
+    pager::{
+        line::{PageLine, RowSpan},
+        page::{Page, RowSpanIter},
+    },
+    reader::{LinePos, QueryLine, Reader},
+    source::Source,
+};
+
+mod line;
+mod page;
+
+#[derive(Debug)]
+pub(crate) struct PageSize {
+    rows: usize,
+    cols: usize,
+}
+
+impl PageSize {
+    pub fn new(rows: usize, cols: usize) -> Self {
+        Self { rows, cols }
+    }
+
+    #[inline]
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct LineMeta {
+    pos: LinePos,
+}
+
+/// Pager clips text lines to fit them in the fixed size frame. The frame is called a page.
+/// Its responsibilites are:
+/// 1. Read source text and load to a page.
+/// 2. Determine lines that are currently "visible" in the page.
+/// 3. Wrap lines based on the column size.
+#[derive(Debug)]
+pub(crate) struct Pager<R, Src> {
+    reader: Reader<R, Src>,
+    size: PageSize,
+    page: Page<LineMeta>,
+}
+
+impl<R, Src: Source<R>> Pager<R, Src> {
+    pub fn new(mut reader: Reader<R, Src>, size: PageSize) -> Result<Option<Self>, AnyError> {
+        match build_page(&mut reader, &size)? {
+            None => Ok(None),
+            Some(page) => Ok(Some(Self { reader, size, page })),
+        }
+    }
+
+    #[inline]
+    pub fn size(&self) -> &PageSize {
+        &self.size
+    }
+
+    pub fn row_spans(&mut self) -> RowSpanIter<'_, LineMeta> {
+        self.page.row_spans()
+    }
+
+    pub fn scroll_down_one_row(&mut self) -> Result<Option<RowSpan<'_>>, AnyError> {
+        if !self.page.move_down_one_row() {
+            let end_pos = self.page.end_line().meta().pos;
+            match self.reader.read_line(QueryLine::NextOf(end_pos))? {
+                None => return Ok(None),
+                Some((pos, text)) => {
+                    let line = PageLine::new(LineMeta { pos }, text, self.size.cols);
+                    self.page.push_back(line);
+                    self.page.move_down_one_row();
+                }
+            }
+        }
+        Ok(Some(self.page.end_row_span()))
+    }
+
+    pub fn scroll_up_one_row(&mut self) -> Result<Option<RowSpan<'_>>, AnyError> {
+        if !self.page.move_up_one_row() {
+            let start_pos = self.page.start_line().meta().pos;
+            match self.reader.read_line(QueryLine::PrevOf(start_pos))? {
+                None => return Ok(None),
+                Some((pos, text)) => {
+                    let line = PageLine::new(LineMeta { pos }, text, self.size.cols);
+                    self.page.push_front(line);
+                    self.page.move_up_one_row();
+                }
+            }
+        }
+        Ok(Some(self.page.start_row_span()))
+    }
+
+    pub fn scroll_to_start(&mut self) -> Result<(), AnyError> {
+        write_start_page(&mut self.reader, &self.size, &mut self.page)?;
+        Ok(())
+    }
+
+    pub fn scroll_to_end(&mut self) -> Result<(), AnyError> {
+        write_end_page(&mut self.reader, &self.size, &mut self.page)?;
+        Ok(())
+    }
+}
+
+fn build_page<R, Src: Source<R>>(
+    reader: &mut Reader<R, Src>,
+    size: &PageSize,
+) -> Result<Option<Page<LineMeta>>, AnyError> {
+    let mut builder = Page::builder(size.rows);
+    let mut lines = reader.lines_from(QueryLine::AtStart);
+    while let Some((pos, text)) = lines.next()? {
+        let line = PageLine::new(LineMeta { pos }, text, size.cols);
+        if !builder.push_back(line) {
+            break;
+        }
+    }
+    match builder.into_page() {
+        Some(page) => Ok(Some(page)),
+        _ => Ok(None),
+    }
+}
+
+fn write_start_page<R, Src: Source<R>>(
+    reader: &mut Reader<R, Src>,
+    size: &PageSize,
+    page: &mut Page<LineMeta>,
+) -> Result<(), AnyError> {
+    let mut writer = page.start_page_writer();
+    let mut lines = reader.lines_from(QueryLine::AtStart);
+    while let Some((pos, text)) = lines.next()? {
+        let line = PageLine::new(LineMeta { pos }, text, size.cols);
+        if !writer.push_back(line) {
+            break;
+        }
+    }
+    writer.write_to_page();
+    Ok(())
+}
+
+fn write_end_page<R, Src: Source<R>>(
+    reader: &mut Reader<R, Src>,
+    size: &PageSize,
+    page: &mut Page<LineMeta>,
+) -> Result<(), AnyError> {
+    let mut writer = page.end_page_writer();
+    let mut lines = reader.lines_rev_from(QueryLine::AtEnd);
+    while let Some((pos, text)) = lines.next()? {
+        let line = PageLine::new(LineMeta { pos }, text, size.cols);
+        if !writer.push_front(line) {
+            break;
+        }
+    }
+    writer.write_to_page();
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use pretty_assertions::assert_eq;
+
+    use crate::{
+        error::AnyError,
+        pager::{PageSize, Pager, line::RowSpan},
+        reader::Reader,
+        source,
+    };
+
+    #[test]
+    fn pager_scroll_up_down() -> Result<(), AnyError> {
+        let s = "abcde\n1234567\nfoo\nbar\n123456789".to_string();
+        let cursor = Cursor::new(s);
+        let source = source::as_readable(cursor);
+        let reader = Reader::new(source);
+
+        let mut pager = Pager::new(reader, PageSize { rows: 3, cols: 8 })?.expect("build pager");
+        assert_eq!(
+            pager.row_spans().collect::<Vec<_>>(),
+            vec![
+                RowSpan::new("abcde", 1),
+                RowSpan::new("1234567", 1),
+                RowSpan::new("foo", 1),
+            ]
+        );
+
+        pager.scroll_down_one_row()?;
+        assert_eq!(
+            pager.row_spans().collect::<Vec<_>>(),
+            vec![
+                RowSpan::new("1234567", 1),
+                RowSpan::new("foo", 1),
+                RowSpan::new("bar", 1),
+            ]
+        );
+
+        pager.scroll_to_end()?;
+        assert_eq!(
+            pager.row_spans().collect::<Vec<_>>(),
+            vec![
+                // Only two row spans since the total rows are 3 (max).
+                RowSpan::new("bar", 1),
+                RowSpan::new("123456789", 2),
+            ]
+        );
+
+        Ok(())
+    }
+}
---
+++ src/pager/line.rs
@@ -0,0 +1,233 @@
+use std::ops::{Bound, RangeBounds};
+
+use ansi_control_codes::parser::{Token, TokenStream};
+use unicode_width::UnicodeWidthChar;
+
+/// A line in a page.
+///
+/// ## About line wrapping on terminal
+/// Modern terminal emulators can wrap a long line automatically if it overflows the width.
+/// But it requires extra consideration when we communicate with a terminal in the raw mode.
+/// For example, if you write a long line at row 1 it continues to row 2 or more until it ends.
+/// In this case, the next line needs to be written after the last row fo the first line,
+/// not simply at row 2.
+///
+/// Alternatively, it is also possible to wrap lines by ourselves and keep all lines
+/// always fit in the terminal width so that a terminal doesn't automatically wrap lines.
+/// This is simpler than the first approach. But in this approach, a terminal cannot know
+/// whether each line actually continues. This affects a behavior of copying.
+///
+/// If a line is automatically wrapped by a terminal, you can copy it as if it is not wrapped:
+/// ```text
+///  │Let's say this line is au│
+///  │tomatically wrapped.     │
+/// ```
+/// Copied text:
+/// ```text
+///  Let's say this line is automatically wrapped.
+/// ```
+/// But if it is wrapped by ourselves, it is copied with a line break:
+/// ```text
+///  │If this line is wrapped manually by th│
+///  │e program, it is copied as two lines. │
+/// ```
+///
+/// Toss implements the former behavior.
+/// It lets a terminal wrap lines while keeping which point a line is wrapped at.
+///
+/// ## Terminology
+/// ```text
+///           Lorem ipsum dolor sit amet, consectetur elit.  ───┬─ sentence
+///           A finibus massa ultricies nec.                 ───┘
+///
+///           ┌ wrap
+///           ├ - - - - - - - - - - - ┐
+///  row ─┬── ❘ Lorem ipsum dolor si  ❘ ──┬─ line slice (per wrap)
+///       ├── ❘ t amet, consectetur   ❘ ──┤
+///       ├── ❘ elit.                 ❘ ──┘
+///       │   └ - - - - - - - - - - - ┘
+///       │   ┌ - - - - - - - - - - - ┐
+///       ├── ❘ A finibus massa ultr  ❘ ──┬─ line slice (per wrap)
+///       └── ❘ icies nec.            ❘ ──┘
+///           └ - - - - - - - - - - - ┘
+/// ```
+#[derive(Debug)]
+pub(super) struct PageLine<Meta> {
+    /// Any metadata related to this line.
+    meta: Meta,
+
+    /// A original sentence this struct refers to.
+    sentence: Sentence,
+
+    /// Wrapping information based on the window width.
+    wrap: Wrap,
+}
+
+impl<Meta> PageLine<Meta> {
+    pub fn new(meta: Meta, text: String, n_cols: usize) -> Self {
+        let sentence = Sentence::new(text);
+        let wrap = Wrap::new(&sentence, n_cols);
+        Self {
+            meta,
+            sentence,
+            wrap,
+        }
+    }
+
+    #[inline]
+    pub fn meta(&self) -> &Meta {
+        &self.meta
+    }
+
+    #[inline]
+    pub fn row_len(&self) -> usize {
+        self.wrap.slices.len()
+    }
+
+    pub fn slice(&self, range: impl RangeBounds<usize>) -> RowSpan<'_> {
+        let start_slice_idx = match range.start_bound() {
+            Bound::Unbounded => 0,
+            Bound::Included(start) => *start,
+            Bound::Excluded(_) => panic!("unexpected excluded bound for slice start"),
+        };
+        let end_slice_idx = match range.end_bound() {
+            Bound::Unbounded => self.wrap.slices.len() - 1,
+            Bound::Included(end) => *end,
+            Bound::Excluded(end) => *end - 1,
+        };
+        let slice_start = &self.wrap.slices[start_slice_idx];
+        let slice_end = &self.wrap.slices[end_slice_idx];
+        let s = &self.sentence.raw[slice_start.start_byte..slice_end.end_byte];
+        let size = end_slice_idx - start_slice_idx + 1;
+        RowSpan::new(s, size)
+    }
+}
+
+#[derive(Debug, Default)]
+struct Sentence {
+    /// An original line including ANSI escape sequences.
+    pub raw: String,
+
+    /// A plain text line without escape sequences.
+    pub plain: String,
+
+    /// A byte index mapping from each character in `plain` to `raw`. Example:
+    /// - raw: `\xb[1mHi\xb[0m, 😀` ("Hi" is bold)
+    /// - plain: `Hi, 😀`
+    /// - plain_to_raw:
+    ///   ```text
+    ///   (escape sequences: bold start)
+    ///   0:  4 ──── H
+    ///   1:  5 ──── i
+    ///   (escape sequences: reset)
+    ///   2: 10 ──── ,
+    ///   3: 11 ──── (space)
+    ///   4: 12 ──┬─ 😀
+    ///   5: 13 ──┤
+    ///   6: 14 ──┤
+    ///   7: 15 ──┘
+    ///   ```
+    pub plain_to_raw: Vec<usize>,
+}
+
+impl Sentence {
+    fn new(raw_line: String) -> Self {
+        let mut plain = String::with_capacity(raw_line.len());
+        let mut plain_to_raw = Vec::new();
+        let mut i_raw = 0;
+        for token in TokenStream::from(&raw_line) {
+            match token {
+                Token::ControlFunction(c) => {
+                    // Allocate a string just to get the byte length of escape sequences :(
+                    i_raw += String::from(c).len();
+                }
+                Token::String(s) => {
+                    plain.push_str(s);
+                    for _ in s.as_bytes() {
+                        plain_to_raw.push(i_raw);
+                        i_raw += 1;
+                    }
+                }
+            }
+        }
+        Self {
+            raw: raw_line,
+            plain,
+            plain_to_raw,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Wrap {
+    /// Slices to fit the original line in the window width.
+    pub slices: Vec<LineSlice>,
+}
+
+impl Wrap {
+    fn new(sentence: &Sentence, n_cols: usize) -> Self {
+        let mut slices = Vec::new();
+
+        let mut n_cells = 0;
+        let mut i_plain_char = 0;
+        let mut i_raw_last_line_end = 0;
+        for c in sentence.plain.chars() {
+            let i_raw = sentence.plain_to_raw[i_plain_char];
+            let cell_width = c.width().unwrap_or(0);
+            n_cells += cell_width;
+            if n_cells > n_cols {
+                slices.push(LineSlice::new(i_raw_last_line_end, i_raw));
+                n_cells = cell_width;
+                i_raw_last_line_end = i_raw;
+            }
+            i_plain_char += c.len_utf8();
+        }
+        slices.push(LineSlice::new(i_raw_last_line_end, sentence.raw.len()));
+
+        Self { slices }
+    }
+}
+
+#[derive(Debug)]
+struct LineSlice {
+    /// A start position in the original raw line.
+    pub start_byte: usize,
+    /// An end position in the original raw line (exclusive).
+    pub end_byte: usize,
+}
+
+impl LineSlice {
+    fn new(start_byte: usize, end_byte: usize) -> Self {
+        Self {
+            start_byte,
+            end_byte,
+        }
+    }
+}
+
+/// Iterator of row spans of the current page.
+/// A row span is neither a row nor a line. It is a wrapped line possibly cut off in the middle
+/// to fit in the page. For example, if the first sentence of the source text is "aabbcc" and the
+/// page column size is 2, the sentence will be broken into 3 rows: "aa", "bb", "cc". If the page
+/// is scrolled down one row, the page will start with "bb" and "cc". This "bbcc" is a row span.
+#[derive(Debug, PartialEq)]
+pub(crate) struct RowSpan<'l> {
+    line: &'l str,
+    size: usize,
+}
+
+impl<'line> RowSpan<'line> {
+    pub(super) fn new(line: &'line str, size: usize) -> Self {
+        Self { line, size }
+    }
+
+    #[inline]
+    pub(crate) fn line(&self) -> &str {
+        self.line
+    }
+
+    #[inline]
+    pub(crate) fn size(&self) -> usize {
+        self.size
+    }
+}
