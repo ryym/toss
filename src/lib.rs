319e1f9b73c8f5e7edc87c9b41c075c850a1529e
ryym <ryym.64@gmail.com>
2025-11-08 15:29:25 +0900

    chore(clippy): Disallow to commit todo!() and dbg!()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 // With the `bench` feature flag, enable unstable test features to run benchmarks.
 // https://doc.rust-lang.org/unstable-book/library-features/test.html#test
 #![cfg_attr(feature = "bench", feature(test))]
+// Clippy configuration. https://rust-lang.github.io/rust-clippy/master/index.html
+#![deny(clippy::dbg_macro)]
+#![deny(clippy::todo)]
 
 pub mod app;
 mod error;
